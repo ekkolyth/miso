19ddccf8581a8db25458b591d4aeed21907eb1f8
Mike Kenway <mike@mikekenway.com>
2026-01-26 20:43:02 -0500

    weird git thing

--- scripts/fmt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-
-cd apps/miso && gofmt -w $(go list -f '{{.Dir}}' ./...)
--- scripts/go.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Build first
-./scripts/build/miso.sh
-
-# Run with remaining arguments
-cd apps/miso && go run ./cmd "$@"
