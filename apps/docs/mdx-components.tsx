59fb2799d118bfec4e19a6d037120d340c4d404b
Mike Kenway <mike@mikekenway.com>
2026-01-26 20:42:07 -0500

    updated docs

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,11 +1,13 @@
 import type { MDXComponents } from "mdx/types";
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs";
 import { CommandSelect } from "@/components/command-select";
+import Miso from "./components/miso";
 
 export function useMDXComponents(components?: MDXComponents): MDXComponents {
     return {
         ...getThemeComponents(),
-        InstallCommand: CommandSelect,
+        CommandSelect,
+        Miso,
         ...components,
     };
 }
