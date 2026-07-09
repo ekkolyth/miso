ddabf19308739015014dc31471d7575ecc0abe2f
Mike Kenway <mike.kenway@wegolook.com>
2026-07-09 10:14:55 -0400

    [release] remove windows from build and distribution

--- scripts/release/create-archives.sh
+++ scripts/release/create-archives.sh
@@ -11,9 +11,7 @@ tar -czf miso_${VERSION}_darwin_amd64.tar.gz -C bin miso-darwin-amd64
 tar -czf miso_${VERSION}_darwin_arm64.tar.gz -C bin miso-darwin-arm64
 tar -czf miso_${VERSION}_linux_amd64.tar.gz -C bin miso-linux-amd64
 tar -czf miso_${VERSION}_linux_arm64.tar.gz -C bin miso-linux-arm64
-zip -j miso_${VERSION}_windows_amd64.zip bin/miso-windows-amd64.exe
 tar -czf misox_${VERSION}_darwin_amd64.tar.gz -C bin misox-darwin-amd64
 tar -czf misox_${VERSION}_darwin_arm64.tar.gz -C bin misox-darwin-arm64
 tar -czf misox_${VERSION}_linux_amd64.tar.gz -C bin misox-linux-amd64
 tar -czf misox_${VERSION}_linux_arm64.tar.gz -C bin misox-linux-arm64
-zip -j misox_${VERSION}_windows_amd64.zip bin/misox-windows-amd64.exe
